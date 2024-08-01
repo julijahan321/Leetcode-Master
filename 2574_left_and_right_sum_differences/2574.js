d90654d40dbd8953f3f8062516668e689be28340
juli-jaan <julijahan.tondra321@gmail.com>
2024-08-01 12:33:21 +0600

    solved:2574

---
+++ 2574_left_and_right_sum_differences/2574.js
@@ -0,0 +1,29 @@
+//problem name leftandrightsumdifferences
+
+var leftRightDifference = function (num) {
+    const LHS = [];
+    const RHS = [];
+    const outPut = []
+    let sum = 0;
+    for (let i = 0; i < num.length - 1; i++) {
+        sum += num[i]
+        LHS.push(sum)
+    }
+    sum = 0;
+    LHS.splice(0, 0, 0)
+    for (let i = num.length - 1; i >= 0; i--) {
+        sum += num[i];
+        RHS.push(sum)
+    }
+    RHS.reverse()
+    RHS.splice(0, 1)
+    RHS.splice(RHS.length, 0, 0)
+    for (let i = 0; i < LHS.length; i++) {
+        for (let j = 0; j < RHS.length; j++) {
+            let sub = Math.abs(LHS[i] - RHS[i]);
+            outPut.push(sub)
+            break;
+        }
+    }
+    return outPut;
+}
