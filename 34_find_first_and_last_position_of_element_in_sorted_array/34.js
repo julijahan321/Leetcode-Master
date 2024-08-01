48c1d02d14cfbfb74161fd8d6a768498b0b66cb4
juli-jaan <julijahan.tondra321@gmail.com>
2024-08-01 20:44:06 +0600

    medium_solved:34

--- 34_find_first_and_last_position_of_element_in_sorted_array/34.js
+++ 34_find_first_and_last_position_of_element_in_sorted_array/34.js
@@ -1,1 +1,27 @@
 //problem name findfirstandlastpositionofelementinsortedarray
+
+//this is my first medium solved
+
+
+var searchRange = function (nums, target) {
+    let outPut = [];
+    let finalOutput = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == target) {
+            outPut.push(i)
+        }
+        
+    }
+    if (outPut.length == 0) {
+        return [-1, -1]
+    }
+    if (outPut.length == 1) {
+        finalOutput[0] = outPut[0]
+        return [finalOutput[0], finalOutput[0]]
+
+    }
+    if(outPut.length>2){
+        return [outPut[0],outPut[outPut.length-1]]
+    }
+    return outPut;
+}
