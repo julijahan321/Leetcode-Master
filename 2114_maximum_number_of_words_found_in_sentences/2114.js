30e2cfa36afdae46402634f743ee485f612f2346
juli-jaan <julijahan.tondra321@gmail.com>
2024-08-05 17:22:51 +0600

    2114.js this file add

--- 2114_maximum_number_of_words_found_in_sentences/2114.js
+++ 2114_maximum_number_of_words_found_in_sentences/2114.js
@@ -1,1 +1,20 @@
 //problem name maximumnumberofwordsfoundinsentences
+
+var mostWordsFound = function (sentences) {
+    let array = [];
+    let output = 0;
+    for (let i = 0; i < sentences.length; i++) {
+        let sum = 1;
+        for (let j = 0; j < sentences[i].length; j++) {
+            if (sentences[i][j] == " ")
+                sum += 1;
+        }
+        array.push(sum)
+    }
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > output) {
+            output = array[i]
+        }
+    }
+    return output;
+};
